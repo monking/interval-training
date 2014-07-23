5ad30018a74a3b309ed5c92f2f7f3fe30e85453f
Christopher Lovejoy <clovejoy@gunnjerkens.com>
2014-07-23 10:35:21 -0700

    cli: named intervals and docs

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ bower install
 
 Create audio files for counting off intervals and reps.
 
-On Ubuntu, install espeak (`sudo apt-get install espeak`), and run:
+On GNU/Linux, install espeak (`sudo apt-get install espeak`), and run:
 ```
 bash audio/generate-audio.sh
 ```
@@ -22,13 +22,31 @@ bash audio/generate-audio.sh say
 ```
 
 ## extra
-You can add more counting sounds by adding a number after the speech program, as in
+You can add more counting sounds by adding a number after the speech program,
+as in:
 ```
-#Ubuntu
+#GNU/Linux
 bash audio/generate-audio.sh espeak 30
 
 #Mac
 bash audio/generate-audio.sh say 30
 ```
 
-If you specify `0`, then you'll enter the interactive timer.
+### CLI
+You can use a command line version of the app by doing the above command while
+specifing `0` as the last parameter.
+
+You will be prompted for sets and intervals. `3 5 30` would give you 3 reps
+with 2 intervals of 5 and 30 seconds. You can also name your intervals, like `3
+5-rest 30-work`, and this name will be called out when the interval starts.
+
+You can also pass your reps and intervals in the initial call:
+```
+bash audio/generate-audio.sh say 0 3 5-rest 30-work
+```
+
+This is easiest if you define an alias:
+```
+alias interval="bash /PATH/TO/REPO/audio/generate-audio.sh say 0"
+interval 3 5-rest 30-work
+```
--- audio/generate-audio.sh
+++ audio/generate-audio.sh
@@ -8,33 +8,61 @@ if [[ $speak_command = espeak ]]; then
   output_option=-w
 else
   output_option=-o
+  output_format=--file-format=WAVE
 fi
 if [[ -z $count ]]; then
   count=20
 fi
 
-o=(th st nd rd th th th th th th);
+function nth() {
+  i=$1
+  o=(th st nd rd th th th th th th);
+  echo ${i}${o[$(($i%10))]}
+}
+
+function say_this() {
+  message=$1
+  output_file=$2
+  if [[ -n $output_file ]]; then
+    output_segment=$output_option $dir/set.wav $output_format
+  fi
+  $speak_command $output_segment $message
+}
+
+function is_integer() {
+  [ "$1" -eq "$1" ] > /dev/null 2>&1
+  return $?
+}
+
 
 if [[ $count != 0 ]]; then
-  $speak_command $output_option $dir/set.wav set
-  $speak_command $output_option $dir/interval.wav interval
+  say_this "set" $dir/set.wav
+  say_this "interval" $dir/interval.wav
 
   for i in $(seq $count); do
-    $speak_command $output_option $dir/count-${i}.wav ${i}${o[$((i%10))]}
-    $speak_command $output_option $dir/${i}.wav ${i}
+    say_this "$(nth $i)" $dir/count-${i}.wav
+    say_this "$i" $dir/${i}.wav
   done
 else
-  echo 'INTERACTIVE MODE (no audio files will be saved)'
-  read -p 'sets, intervals: ' sets intervals
+  if [[ -n $3 ]]; then
+    shift 2
+    read sets intervals <<< $@
+  else
+    echo 'INTERACTIVE MODE (no audio files will be saved)'
+    read -p 'sets, intervals: ' sets intervals
+  fi
   s=0
   while [[ $s -lt $sets ]]; do
     s=$(($s+1))
     i=0
     for interval in $intervals; do
       i=$(($i+1))
-      $speak_command "${i}${o[$((i%10))]} interval, $interval seconds, set $s" &
+      time=$(sed -E 's/^([0-9]+)-?.*$/\1/' <<< $interval)
+      message=$(sed -E 's/^[0-9]+-?(.*)$/\1/' <<< $interval)
+      if [[ -z $message ]]; then message="$(nth $i) interval"; fi
+      say_this "$message, $time seconds, set $s" &
       sleep $interval
     done
   done
-  $speak_command "DONE!" &
+  say_this "DONE!" &
 fi
